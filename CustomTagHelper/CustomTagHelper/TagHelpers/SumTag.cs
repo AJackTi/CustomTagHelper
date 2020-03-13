fce95c97650f503492ba23e4008e2c224ddd37a3
AJackTi <AJackTi@AJACKTI>
2020-03-13 16:38:44 +0700

    Add Project CustomTagHelper

---
+++ CustomTagHelper/CustomTagHelper/TagHelpers/SumTag.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomTagHelper.TagHelpers
+{
+    [HtmlTargetElement("sum", TagStructure = TagStructure.NormalOrSelfClosing)]
+    public class SumTag : TagHelper
+    {
+        [HtmlAttributeName("a")]
+        public double a { get; set; }
+
+        [HtmlAttributeName("b")]
+        public double b { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "";
+            output.Content.SetHtmlContent((a + b).ToString());
+        }
+    }
+}
